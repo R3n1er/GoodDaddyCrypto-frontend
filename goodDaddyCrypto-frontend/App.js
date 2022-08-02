9f555d84c547d3878115cf2552b4201932407b37
Alban <renier.alban@gmail.com>
2022-08-02 14:32:21 +0200

    modification sig-in screen

--- goodDaddyCrypto-frontend/App.js
+++ goodDaddyCrypto-frontend/App.js
@@ -1,36 +1,30 @@
-import { StyleSheet, Text, View, Icon } from 'react-native';
+import { StyleSheet, Text, View, Icon } from "react-native";
 
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
-
-
 // Imports des screens
-import HomeScreen from './screens/HomeScreen';
-import DashboardScreen from './screens/DashBoardScreen';
-import GuidesScreen from './screens/GuidesScreen';
-import StrategiesScreen from './screens/StrategiesScreen';
+import HomeScreen from "./screens/HomeScreen";
+import DashboardScreen from "./screens/DashBoardScreen";
+import GuidesScreen from "./screens/GuidesScreen";
+import StrategiesScreen from "./screens/StrategiesScreen";
 import TransactionsScreen from "./screens/TransactionsScreen";
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 //Imports de la navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 // Création du composant BottomTabNavigator
 const BottomNavigator = () => {
-
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
         tabBarIcon: ({ color }) => {
-
           let iconName;
           // Routes des éléments du menu en correspondance avec les icones FontAwesome
           if (route.name == "StrategiesScreen") {
@@ -60,32 +54,45 @@ const BottomNavigator = () => {
       <Tab.Screen name="Transactions" component={TransactionsScreen} />
     </Tab.Navigator>
   );
-}
+};
 
 // on peut créer une page dans stratégie avec une fonction stratégie
 
 // Début return fonction App
 export default function App() {
-
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title:'Page Home' }}/>
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ title:'Register' }}/>
-        <Stack.Screen name="Login" component={LoginScreen} options={{ title:'Login' }}/>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Good Daddy Crypto" }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ title: "Register" }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Login" }}
+        />
         <Stack.Screen name="BottomNavigator" component={BottomNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-
 }
 
 // Styles CSS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
