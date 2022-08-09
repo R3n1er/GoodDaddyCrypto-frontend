0b0719a36bf3d115d6225f483c4afec7d6bbd00c
Alban <renier.alban@gmail.com>
2022-08-09 16:28:42 +0200

    modification wallet ETH

--- goodDaddyCrypto-frontend/screens/wallets/ethWalletScreen.js
+++ goodDaddyCrypto-frontend/screens/wallets/ethWalletScreen.js
@@ -1,42 +1,79 @@
 import React, { useEffect, useState } from "react";
 
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  SafeAreaView,
-  TouchableOpacity,
-  Image,
-} from "react-native";
+import { StyleSheet, View, SafeAreaView, TouchableOpacity } from "react-native";
+// Import React Native Elements
+import { Text, Card, Button, Image } from "@rneui/themed";
+
+// Import de la fonction GetCrypto Current Price
+// import {getCryptoPriceData} from '../../services/GetCryptoPriceService';
+
 // Icones
 import Icon from "react-native-vector-icons/FontAwesome";
 // React Redux
 import { connect } from "react-redux";
 
+// AXIOS pour requette API
+import axios from "axios";
+
 const ethWallet = (props) => {
-  const asset = BTC;
+  // Declaration variable d'etat pour afficher le prix
+
+  const [showprice, setShowPrice] = useState(0);
+
+  // Declaration de variables
+  const assetName = "Ethereum";
+  const assetSymbol = "ETH";
+  const cryptoAssetId = "ethereum";
+  const assetLogo =
+    "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_16/f231d7382689406f9a50dde841418c64.png";
+
+  // Use effect
+  useEffect(() => {
+    // Fonction pour récupérer le prix d'un asset avec API Coingecko
+    async function getCryptoPriceData() {
+      try {
+        // This is where the api call will go
+        const response = await axios.get(
+          `https://api.coingecko.com/api/v3/simple/price?ids=${cryptoAssetId}&vs_currencies=eur`
+        );
+        // Retour de la fonction
+        console.log("typeof response", typeof response.data.ethereum.eur);
+
+        setShowPrice(response.data.ethereum.eur);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getCryptoPriceData();
+  }, []);
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Prix actuel du {asset}</Text>
-        <Text>
-          <Image
-            source={
-              "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_16/f231d7382689406f9a50dde841418c64.png"
-            }
-          />
-          Bitcoin
-        </Text>
-        <Text>Symbol BTC</Text>
-        <Text> Price BTC $</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.title}>
+        <Text>Wallet {assetName}</Text>
       </View>
-{/* // Insert du graphique ici */}
-      <View>ICI LA PLACE DU GRAPHIQUE</View>
+      <Card>
+        <Card.Title>Prix actuel du {assetName}</Card.Title>
+        <Image source={{ assetLogo }} />
+        <Card.Divider />
+        {/*//get current Price of asset with API */}
+        <Text>
+          1 {assetSymbol} = {showprice} €
+        </Text>
+      </Card>
+      {/* // Insert du graphique ici 
+      Le graphique doit afficher l'évolution du portefeuille dans le temps en fonction de amount of token et price$*/}
+      <View>
+        <Text>ICI LA PLACE DU GRAPHIQUE</Text>
+        {/* <walletChart /> */}
+      </View>
       {/* // Ici information sur la valeur du portefeuille */}
       <View>
-        <Text>Valeur de ton portefeuille {asset} : 800 €</Text>
+        <Card>
+          <Text>Valeur de ton portefeuille {assetName}</Text>
+          <Text>800 €</Text>
+          <Text>soit 0,051 {assetSymbol}</Text>
+        </Card>
       </View>
     </SafeAreaView>
   );
@@ -74,4 +111,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ethWallet;
+export default btcWallet;
